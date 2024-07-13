280658e1df5fa21b4991df4d679c3a3fbc29b2e6
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2024-07-13 09:12:14 +0200

    cmake: improve git-version-gen.cmake

--- cmake/git-version-gen.cmake
+++ cmake/git-version-gen.cmake
@@ -7,10 +7,15 @@ if (EXISTS "${CMAKE_SOURCE_DIR}/version.cmake")
     include("${CMAKE_SOURCE_DIR}/version.cmake")
 elseif(EXISTS "${CMAKE_SOURCE_DIR}/.git" AND Git_FOUND)
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} describe --abbrev=4 HEAD
+        COMMAND
+            ${GIT_EXECUTABLE}
+                describe
+                    --abbrev=4
+                    --match "v[0-9]*"
+                    HEAD
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
         OUTPUT_VARIABLE _gvg_version
         ERROR_VARIABLE _gvg_version_err
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
         RESULT_VARIABLE _gvg_version_result
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
@@ -20,27 +25,70 @@ elseif(EXISTS "${CMAKE_SOURCE_DIR}/.git" AND Git_FOUND)
     endif()
 
     if(NOT _gvg_version_result EQUAL 0)
-        message(FATAL_ERROR "Failed to find version from Git. Git process returned ${_gvg_version_result}")
+        message(FATAL_ERROR "Failed to find version from Git: Git process returned ${_gvg_version_result}")
     endif()
 
-    string(REGEX REPLACE "^v" "" _gvg_version "${_gvg_version}")
-    string(REGEX REPLACE "^([^-]*)-(.*)" "\\1.\\2" _gvg_version "${_gvg_version}")
-    string(REGEX REPLACE "-g" "-" PACKAGE_VERSION "${_gvg_version}")
+    if(NOT _gvg_version)
+        message(FATAL_ERROR "Failed to find version from Git: Empty")
+    endif()
+
+    string(REPLACE "-" ";" _gvg_version_split "${_gvg_version}")
+    list(LENGTH _gvg_version_split _gvg_version_split_len)
+    if(_gvg_version_split_len)
+        list(GET _gvg_version_split 0 PACKAGE_VERSION_CANONICAL)
+        string(REGEX REPLACE "^v" "" PACKAGE_VERSION_CANONICAL "${PACKAGE_VERSION_CANONICAL}")
+        set(PACKAGE_VERSION "${PACKAGE_VERSION_CANONICAL}")
+        if(_gvg_version_split_len GREATER 1)
+            list(GET _gvg_version_split 1 _gvg_version_rev)
+            set(PACKAGE_VERSION "${PACKAGE_VERSION}.${_gvg_version_rev}")
+            if(_gvg_version_split_len GREATER 2)
+                list(GET _gvg_version_split 2 _gvg_version_hash)
+                string(REGEX REPLACE "^g" "" _gvg_version_hash "${_gvg_version_hash}")
+                set(PACKAGE_VERSION "${PACKAGE_VERSION}-${_gvg_version_hash}")
+            endif()
+        endif()
+    endif()
+
+    if(NOT PACKAGE_VERSION_CANONICAL)
+        message(FATAL_ERROR "Failed to find canonical version")
+    endif()
+
+    execute_process(
+        COMMAND
+            ${GIT_EXECUTABLE}
+                update-index
+                    -q
+                    --refresh
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    )
+
+    execute_process(
+        COMMAND
+            ${GIT_EXECUTABLE}
+                diff-index
+                    --name-only
+                    HEAD
+                    --
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        OUTPUT_VARIABLE _gvg_diff_index
+        ERROR_VARIABLE _gvg_diff_index_err
+        RESULT_VARIABLE _gvg_diff_index_result
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    if(NOT _gvg_diff_index_err AND NOT _gvg_diff_index_result AND _gvg_diff_index)
+        set(PACKAGE_VERSION "${PACKAGE_VERSION}-dirty")
+    endif()
 else()
     message(FATAL_ERROR "Can't find version information!")
 endif()
 
-string(REGEX MATCHALL "[0-9]+" _gvg_version_list "${PACKAGE_VERSION}")
-list(LENGTH _gvg_version_list _gvg_version_list_len)
-if(NOT _gvg_version_list_len GREATER_EQUAL 2)
-    message(FATAL_ERROR "Invalid version: ${PACKAGE_VERSION}")
-endif()
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/cpack/install_version.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/install_version.cmake
+    @ONLY
+)
 
-list(GET _gvg_version_list 0 _gvg_version_major)
-list(GET _gvg_version_list 1 _gvg_version_minor)
-set(PACKAGE_VERSION_CANONICAL "${_gvg_version_major}.${_gvg_version_minor}")
-
-if(EXISTS "${CMAKE_SOURCE_DIR}/.git" AND _gvg_version_list_len GREATER_EQUAL 3)
-    list(GET _gvg_version_list 2 _gvg_version_patch)
-    set(PACKAGE_VERSION_CANONICAL "${PACKAGE_VERSION_CANONICAL}.${_gvg_version_patch}")
-endif()
+list(APPEND CPACK_INSTALL_SCRIPTS
+    "${CMAKE_CURRENT_BINARY_DIR}/install_version.cmake"
+)
